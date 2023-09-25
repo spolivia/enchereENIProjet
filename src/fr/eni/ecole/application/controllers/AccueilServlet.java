d186139b1e052ae9736c37a1af77f0719d168b4f
Greggory Picoli <greggorypicoli@gmail.com>
2023-09-25 08:59:30 +0200

    Update AccueilServlet.java

--- src/fr/eni/ecole/application/controllers/AccueilServlet.java
+++ src/fr/eni/ecole/application/controllers/AccueilServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/listeArticles")
-public class ListArticlesServlet extends HttpServlet {
+public class AccueilServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
     private ArticlesManager articlesManager;
